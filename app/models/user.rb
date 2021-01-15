7da6f8683251277139fa8570f574a3669250b402
2DN <74901304+2DN@users.noreply.github.com>
2021-01-15 17:09:50 +0900

    with_optionsでバリデーションをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,21 @@ class User < ApplicationRecord
   VALID_KANJI_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/
   VALID_KATAKANA_REGEX = /\A[ァ-ヶ]+\z/
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name_kanji
+    validates :first_name_kanji
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :date_of_birth
+  end
+
+  
   validates_format_of :email, with: VALID_EMAIL_REGEX
   validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください!'
-  validates_format_of :last_name_kanji, presence: true, with: VALID_KANJI_REGEX, message: 'には全角文字を使用してください'
-  validates_format_of :first_name_kanji, presence: true, with: VALID_KANJI_REGEX, message: 'には全角文字を使用してください'
-  validates_format_of :last_name_kana, presence: true, with: VALID_KATAKANA_REGEX, message: 'には全角カタカナを使用してください'
-  validates_format_of :first_name_kana, presence: true, with: VALID_KATAKANA_REGEX, message: 'には全角カタカナを使用してください'
-  validates :date_of_birth, presence: true
+  validates_format_of :last_name_kanji, with: VALID_KANJI_REGEX, message: 'には全角文字を使用してください'
+  validates_format_of :first_name_kanji, with: VALID_KANJI_REGEX, message: 'には全角文字を使用してください'
+  validates_format_of :last_name_kana, with: VALID_KATAKANA_REGEX, message: 'には全角カタカナを使用してください'
+  validates_format_of :first_name_kana, with: VALID_KATAKANA_REGEX, message: 'には全角カタカナを使用してください'
+
 end
