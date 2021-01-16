1f287f4e8d6607767c8b314825916e0f4ea2f408
2DN <74901304+2DN@users.noreply.github.com>
2021-01-16 11:06:30 +0900

    categoryモデル作成

--- db/migrate/20210116015916_create_items.rb
+++ db/migrate/20210116015916_create_items.rb
@@ -5,6 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :description, null: false
       t.integer :price, null: false
       t.references :user, null: false, foreign_key: true
+      t.integer :category_id, null: false
       t.timestamps
     end
   end
