cc6aedc1278996b506fb64596e7e01f153e4e6df
2DN <74901304+2DN@users.noreply.github.com>
2021-01-16 11:20:12 +0900

    itemsテーブル　バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -6,4 +7,27 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_taken
 
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :days_taken_id
+    validates :price
+    validates :user
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :days_taken_id
+  end
+
+  validates :name, length: {maximum: 40}
+  validates :description, length: {maximum: 1000}
+  validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
 end
