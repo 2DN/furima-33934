961807919dbb11f8d9a66fa3193c8d215c7eea01
2DN <74901304+2DN@users.noreply.github.com>
2021-01-17 10:51:19 +0900

    destroyアクション内　インスタンス削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only:[:edit, :update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -36,8 +36,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
-  
+
 
 
   private
