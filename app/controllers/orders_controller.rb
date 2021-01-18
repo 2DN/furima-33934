4ea2ebcd364c62750edb390e24f4324beea60860
2DN <74901304+2DN@users.noreply.github.com>
2021-01-18 23:26:57 +0900

    move_to_index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @item = Item.find(params[:item_id])
@@ -33,5 +34,9 @@ class OrdersController < ApplicationController
       card: order_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def move_to_index
+    redirect_to root_path if @item.user == current_user
   end
 end
