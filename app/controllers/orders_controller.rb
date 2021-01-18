cc59c6aa3b92325187d4acfc14f07a91f7bf808d
2DN <74901304+2DN@users.noreply.github.com>
2021-01-18 23:33:50 +0900

    authenticate_user! 修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :create]
+  before_action :authenticate_user!
   before_action :move_to_index, only: [:index, :create]
 
   def index
