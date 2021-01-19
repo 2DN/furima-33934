b3d510bfc8b46c0ae9a8ca40238add48e4b8fc44
2DN <74901304+2DN@users.noreply.github.com>
2021-01-19 11:12:11 +0900

    example user_idが空の場合　追加

--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -75,6 +75,14 @@ RSpec.describe ItemOrder, type: :model do
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include("Token can't be blank")
       end
+
+      it 'user_idが空の場合' do
+        @item_order.user_id = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("User can't be blank")
+      end
+
+      
     end
   end
 end
