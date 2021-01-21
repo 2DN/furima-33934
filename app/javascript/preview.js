1fadcaa11c6e0854a12ee1426a8a8e5a9193a5a2
2DN <74901304+2DN@users.noreply.github.com>
2021-01-21 14:27:13 +0900

    preview.js リファクタリング

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,23 +1,30 @@
 if (document.URL.match(/new/) || document.URL.match(/edit/)){
   document.addEventListener('DOMContentLoaded', function(){
+
     const ImageList = document.getElementById("image-list");
-    document.getElementById("item-image").addEventListener("change", function(e){
+    
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div')
+      const blobImage = document.createElement('img');
+
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('class', 'image-size');
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+    
+    document.getElementById("item-image").addEventListener('change', function(e){
       const imageContent = document.querySelector('img');
       if(imageContent){
         imageContent.remove();
       }
-      
-      const file = e.target.files[0];
+
+      const file = e.target.files[0]
       const blob = window.URL.createObjectURL(file);
 
-      const imageElement = document.createElement('div');
-      const blobImage = document.createElement('img');
-
-      blobImage.setAttribute('src', blob);
-      blobImage.setAttribute('class', 'image-size')
-      
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
+      createImageHTML(blob);
     });
+    
   });
 }
